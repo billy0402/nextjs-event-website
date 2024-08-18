a95d998a99856eb4f5dfef805fdca78cbaa60a80
Billy Huang <10646003@ntub.edu.tw>
2024-08-18 12:41:50 +0800

    feat(app): setup chakra ui

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,15 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
+import { ColorModeScript } from '@chakra-ui/react';
+
+import theme from '@/theme';
+
 const Document = () => {
   return (
     <Html lang='en'>
       <Head />
       <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
       </body>
